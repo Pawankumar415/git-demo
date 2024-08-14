784a59ef99e5f88dd16804ccb4e4b15e09f54603
pawan kumar <pawankounder415@gmail.com>
2024-08-14 15:00:31 +0530

    new commit

---
+++ dem1.py
@@ -0,0 +1,362 @@
+# two types of comments available in python
+
+# single line comment  '#'
+# multi line comment
+
+# this is simple function its used parameter to print the name 10 times when you called the function
+# def my_function(name):
+#     for i in range(10):
+#         print(f"hello {name}")
+#
+# my_function("pawan kumar")
+
+
+# snklnsklvskl;mnvdfnlbnd
+# ml;ksmvbkl;dm;mfn;mnlm
+# kkl;dfk;lbm;dgmh
+# jmkl;sfm;lbmdl;cg
+# mklmkvbl;dgmn
+# klm;kmbn ;kcf
+# mkldmk;bmfgn
+
+
+"""
+ghkmgbjlh
+sfhnfgk,gl,j.h
+hnflvskljglsf
+lkanfjklvnsklnhg
+alkngvsfklbhmdh
+an dgjkvnjsnbhf
+kjadbngjklvnsfljkbh
+sbkvbsfnb
+ajbngjkvnsjknbh
+lanzdgklvnslbh
+
+"""
+
+# def my_function(name):
+#     for i in range(10):
+#         print(f"hello {name}")
+#
+# my_function("pawan kumar")
+
+
+# This is will print hello world
+# print("Hello, World!")
+
+"""
+xcbdgjnvm
+"""
+
+'''
+gcmnfvhmfg,
+'''
+
+"""
+ghkmgbjlh
+sfhnfgk,gl,j.h
+hnflvskljglsf
+lkanfjklvnsklnhg
+alkngvsfklbhmdh
+an dgjkvnjsnbhf
+kjadbngjklvnsfljkbh
+sbkvbsfnb
+ajbngjkvnsjknbh
+lanzdgklvnslbh
+
+"""
+
+a = """ hasvksdjnvbjk fjkb df
+janjkvnsjkdbn jkf
+asjvhnjksdzvbkxdfb
+vjadvb jsbf
+sbhfkcsdh
+"""
+
+# print(a)
+
+
+# print("pawan")
+# print("sarath")
+# phone = 7506257415
+# print(phone)
+
+# name="rishabh"
+# name1="ayush"
+# print(name1)
+# print(name)
+
+# name = "pawan"
+# Name = "kumar"
+# print(name)
+
+
+# a = 25
+# A = 28
+# print(A)
+
+
+
+# name="Rishabh dubey"
+# age=21
+# phone=6388248689
+#
+# print(name)
+# print(age)
+# print(phone)
+
+
+# data = "pawan"
+# data = 25
+# print(data)
+
+
+# int to float
+
+num1 = 24
+# print(float(num1))
+data = float(num1)
+# print(data)
+
+
+#  float to int
+
+# num1 = 24.8
+# #print(int(num1))
+# num1=int(num1)
+# print(num1)
+
+
+# data = "rishabh"
+# # print(int(data))
+# # data=int(data)
+# # print(type(data))
+# # print(float(data))
+# print(int(data))
+#
+#
+# None1 = "pawan"
+
+# first_name="rishabh"
+# last_name="dubey"
+# name5 = first_name+" "+last_name
+# print(name5)
+
+
+
+# python strings
+
+#
+# name = "pawan"
+# name = 'pawan'
+# data = "my name is 'pawan' "
+# print(data)
+
+
+# data = "hellopython"
+# print(data[5])
+# print(data[8])
+
+# print(len(data))
+# in operator
+
+# print('' not in data) # it will return boolean value
+
+
+
+# data = "hello PYTHON"
+# print(data[-4])
+# print(data[5:11])
+# print(data[0:6:5])
+
+# print(data[5:])
+# print(data[-6:-3])
+# print(data.upper())
+# print(data.lower())
+# print(data.capitalize())
+# print(data.title())
+# data="      subject       "
+# print(data.strip())
+
+
+# name="rishbh"
+# print(name.replace("h","b",))
+
+
+# names = "pawan$dinesh$suresh$kumar$rishab$raja"
+# print(names.split('$'))
+#
+# data = "1*2*3"
+#
+# print(data.split('*'))
+
+#
+# a = "Hello "
+# b = "World"
+# c = a + b
+# print(c)
+
+
+
+
+
+
+
+
+
+
+name = "Raj"
+# format string
+# f-string
+
+
+# format string
+
+
+# print("my name is: {}".format(name))
+# print(f"my name is: {name}")
+
+# name="ravi"
+# age="21"
+# phone="6388248756"
+# print("my name is:{}".format(name))
+# print("my age is:{}".format(age))
+# print("my phone is:{}".format(phone))
+
+
+# for i in range(10):
+#     print(i)
+#     if i == 7:
+#         print("loop finish")
+#         break
+
+
+
+#print(5 < 10)
+# and  if the two condition must True
+
+# print(not(5 < 7 and 4 < 10))
+
+# in
+
+# data = "12345"
+# print('10' not in data)
+
+
+
+# if condtion:
+#     statement
+
+
+# age = 5
+#
+# if age > 18:
+#     print("apply for driving licence")
+# else:
+#     print("not able to apply")
+
+
+# marks = 10
+#
+# if marks > 90:
+#     print("A grade")
+# elif marks > 70:
+#     print("B grade")
+# elif marks > 50:
+#     print("C grade")
+# else:
+#     print("FAIL")
+
+# if conddition  ye compulsory run hoga
+# input() str type
+# a = input("enter the first number: ")
+# b = input("enter the second number: ")
+
+
+
+# day= int(input("enter the number :"))
+# if day==1 :
+#     print('monday')
+# elif day==2:
+#     print("tuesday")
+# elif day==3:
+#     print("wedenesday")
+# elif day==4:
+#     print("thrusday")
+# elif day==5:
+#     print("friday")
+# elif day==6:
+#     print("saturday")
+# elif day==7:
+#     print("sunday")
+# else:
+#     print("invalid")
+
+# year = int(input("Enter a year: "))
+#
+# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print(year, "is a leap year")
+# else:
+#     print(year, "is not a leap year")
+# print(2022 % 4 == 0)
+#
+# print(2022 % 100 != 0)
+# print(2022 % 400 == 0)
+
+# print(True or False)
+
+# number = int(input("enter the number: "))
+# if number>0:
+#     print("number is positive")
+# elif number<0:
+#     print("number is negative")
+# elif number==0:
+#     print("number is equal to zero")
+# else :
+#     print("fail")
+
+# number=int(input("enter the number :"))
+# if number%2==0:
+#     print("is a even number")
+# else:
+#     print("is a odd number")
+
+# vowels = "aeiou"
+# user= input("enter the character: ")
+# if user in vowels:
+#     print(user,"is vowel")
+# else:
+#     print(user,"is not vowel")
+
+# num1 = float(input("enter the number: "))
+# num2 = float(input("enter the number: "))
+# num3 = float(input("enter the number: "))
+# if num1>= num2 and num1>=num3:
+#     largest=num1
+# elif num2>= num1 and num2>=num3:
+#     largest=num2
+# else:
+#     largest=num3
+# print("the largest number:", largest)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ new1.py
@@ -0,0 +1,53 @@
+# Python is a high-level, interpreted,
+# and general-purpose dynamic programming language that
+# focuses on code readability. It is an excellent language for students to
+# learn due to its simplicity, flexibility, and versatility.
+
+
+# Here are the advantages of Python for students:
+#
+# Easy to Learn: Python has a simple syntax and is relatively easy to learn,
+# making it a great language for beginners.
+
+
+# High-Level Language: Python is a high-level language,
+# meaning it abstracts low-level details, making it more user-friendly.
+
+# Versatile: Python can be used in a broad range of environments,
+# including data mining, data science, machine learning, web development,
+# web frameworks, embedded systems, graphic design, gaming, network development, and more
+#
+# .
+# Extensive Support Libraries: Python boasts extensive support
+# libraries like NumPy for numerical calculations and Pandas
+# for data analytics, making it suitable for scientific and data-related applications.
+
+
+# Open Source and Large Active Community Base: Python is open source,
+# and it has a large and active community that contributes to its development and provides support.
+
+
+# User-Friendly Data Structures: Python offers intuitive and
+# easy-to-use data structures, simplifying data manipulation and management.
+
+
+# Dynamically Typed Language: Python is dynamically typed, meaning
+# you don’t need to declare data types explicitly, making it flexible but still reliable.
+
+
+# Minimal Setup: For beginners, the syntax of other
+# languages may seem confusing. The minimal setup and readability of Python
+# allow you to think like a programmer and save time.
+
+
+# Learning Resources Abound: Python is an open-source
+# language that can be used by anyone. Online programming courses and tutorials
+# are plentiful, ensuring that you never stop learning new codes and
+# experimenting with your older ones.
+
+
+# Python is Highly Extensible: New additions can be extended or
+# included with minimal to no change in the existing code.
+
+# Some popular companies that use Python include Dropbox,
+# Microsoft, Cisco, Spotify, Quora, and Facebook.
